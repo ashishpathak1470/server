7f091040e3fb4372fdfb1346ce502f73363f4f35
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-06-02 22:16:20 +0530

    delete

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,27 @@ const PORT = 3000;
 const SECRET_KEY = "your_secret_key";
 const axios = require("axios");
 
+// CORS configuration
+const corsOptions = {
+  origin: "https://movie-library-bay.vercel.app",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    return res.status(200).json({});
+  }
   next();
 });
-
-app.use(cors(
-  {
-    origin: "",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
-  }
-));
 
 mongoose
   .connect(
